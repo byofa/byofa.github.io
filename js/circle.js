4640f5d42d0822963e121506f532bd76c4487a4c
Fabio Alexandre Aires Machado <72966805+Byofa@users.noreply.github.com>
2021-04-16 05:04:26 +0200

    responsive all
    responsive bodies and title, implement different event on add bodies

---
+++ js/circle.js
@@ -0,0 +1,9 @@
+var circle = document.getElementById("circle");
+// console.log("height before :"+circle.offsetHeight);
+// console.log("width before :"+circle.offsetWidth);
+circle.style.width = circle.offsetHeight+"px";
+circle.style.borderBottomLeftRadius = circle.offsetHeight+"px";
+circle.style.borderTopLeftRadius = circle.offsetHeight+"px";
+// console.log("width after :"+circle.offsetWidth);
+var profil = document.getElementById("profil");
+// console.log("width profil :"+profil.offsetWidth);
